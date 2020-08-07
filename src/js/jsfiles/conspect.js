b7911515434b95335b3faf9c131ef8fcc4aa1419
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-07 22:58:33 +0300

    04

--- src/js/jsfiles/conspect.js
+++ src/js/jsfiles/conspect.js
@@ -1,5 +1,5 @@
 let consp = document.querySelector(".conspect__text");
-let btn = document.querySelector(".nav__button");
+let btn = document.querySelector(".nav__button--type");
 
 btn.addEventListener("click", function () {
   consp.style.display = "inline";
