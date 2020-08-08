45d80797f00c9fbc645bccaceb056d73197f9727
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-08 20:13:26 +0300

    добавил перем

--- src/js/jsfiles/conspect.js
+++ src/js/jsfiles/conspect.js
@@ -1,6 +1,12 @@
-let consp = document.querySelector(".conspect__text");
+let consp = document.querySelector(".conspect__text--type");
 let btn = document.querySelector(".nav__button--type");
+let btnPerem = document.querySelector(".nav__button--perem");
+let conspPerem = document.querySelector(".conspect__text--perem");
 
 btn.addEventListener("click", function () {
   consp.style.display = "inline";
+});
+
+btnPerem.addEventListener("click", function () {
+  conspPerem.style.display = "inline";
 });
